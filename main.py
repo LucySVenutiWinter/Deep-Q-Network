b0b45d064c5182cc3206a729e4483fd2dde60092
Lucy S Venuti Winter <lucy.s.venuti@gmail.com>
2022-05-17 19:45:26 -0700

    Number of trials can now be passed in.

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import dqn
 
 import sys
 
-EPISODES = 10000
 ENVIRONMENT = "VizdoomDefendLine-v0"
 
 env = gym.make(ENVIRONMENT)
@@ -14,23 +13,35 @@ env = gym.make(ENVIRONMENT)
 state = env.reset()
 
 def print_help():
-    print(f"Usage: {sys.argv[0]} <mode> <name>")
+    print(f"Usage: {sys.argv[0]} <mode> <value> <name>")
     print("Mode must be either \"train\" or \"eval\"")
+    print("If in train mode, train for value episodes")
+    print("If in eval mode, show value episodes")
     print("If name is not given, \"dqn_default\" is used")
 
-if len(sys.argv) > 1 and len(sys.argv) < 4:
-    if len(sys.argv) < 3:
+if len(sys.argv) > 1 and len(sys.argv) < 5:
+    if len(sys.argv) < 4:
         name = "dqn_default"
     else:
-        name = sys.argv[2]
+        name = sys.argv[3]
+
+    try:
+        num_in = int(sys.argv[2])
+    except:
+        if sys.argv[1] == "eval":
+            num_in = 10
+            print(f"Given no or bad number, assuming {num_in}")
+        elif sys.argv[1] == "train":
+            num_in = 10000
+            print(f"Given no or bad number, assuming {num_in}")
 
     if sys.argv[1] == "eval":
         dqnet = dqn.DQN(name=name)
         dqnet.training = False
-        train.test_run(10, dqnet, env, render=True)
+        train.test_run(num_in, dqnet, env, render=True)
     elif sys.argv[1] == "train":
         dqnet = dqn.DQN(name=name)
-        train.train_episodes(EPISODES, dqnet, env, log=True)
+        train.train_episodes(num_in, dqnet, env, log=True)
     else:
         print_help()
 else:
